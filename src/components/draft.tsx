2c6a64d51469c11d53786b7ef9650ba534d4af72
Ivan Kolontaevsky <guiteache@gmail.com>
2023-10-07 00:39:14 +0300

    new animation added

--- src/components/draft.tsx
+++ src/components/draft.tsx
@@ -21,6 +21,25 @@ const MegaPower: React.FC = () => {
 
   const beautyWrapRef = useRef<HTMLDivElement>(null);
 
+  const rightSideWrapRef = useRef<HTMLDivElement>(null);
+  const decisionContentRef = useRef<HTMLDivElement>(null);
+  const decisionLetterstRef = useRef<HTMLDivElement>(null);
+  const decisionImgRef = useRef<HTMLDivElement>(null);
+
+  const classicContentRef = useRef<HTMLDivElement>(null);
+  const classicLetterstRef = useRef<HTMLDivElement>(null);
+  const classicImgRef = useRef<HTMLDivElement>(null);
+
+  const elementaryContentRef = useRef<HTMLDivElement>(null);
+  const elementaryLetterstRef = useRef<HTMLDivElement>(null);
+  const elementaryImgRef = useRef<HTMLDivElement>(null);
+
+  const energyContentRef = useRef<HTMLDivElement>(null);
+  const energyLetterstRef = useRef<HTMLDivElement>(null);
+  const energyImgRef = useRef<HTMLDivElement>(null);
+
+  const lastBlocktRef = useRef<HTMLDivElement>(null);
+
 
   gsap.registerPlugin(ScrollTrigger);
 
@@ -29,57 +48,92 @@ const MegaPower: React.FC = () => {
 
       const flagmanItems = gsap.utils.toArray(".js-megaPower-flagman-item");
       const beautyItems = gsap.utils.toArray(".js-beauty-item");
+      const lastItems = gsap.utils.toArray(".js-last-item");
 
-      const megaTL = gsap.timeline({paused: true});
-      megaTL.fromTo(titleRef.current, {}, {duration: .3});
-      megaTL.fromTo(titleRef.current, {autoAlpha: 1}, {autoAlpha: 0, duration: .2});
-      megaTL.fromTo(subtitleRef.current, {autoAlpha: 0}, {autoAlpha: 1, duration: .5});
-      megaTL.fromTo(circleRef.current, {yPercent: 50, y: `${window.innerHeight / 2}`, scale: .7}, {yPercent: 0, y: 0, scale: 10, duration: 6}, '-=.7');
-      megaTL.fromTo(megaImageRef.current, {yPercent: 0, autoAlpha: 1}, {yPercent: -40,  duration: 5}, '-=5.7');
-      megaTL.to(subtitleRef.current, {autoAlpha: 0, duration: .5});
-      megaTL.fromTo(flagmanItems, {autoAlpha: 0}, {autoAlpha: 1, duration: 1, stagger: 1});
+      const megaTL = gsap.timeline({ paused: true });
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+      megaTL.fromTo(titleRef.current, { autoAlpha: 1 }, { autoAlpha: 0, duration: .2 });
+      megaTL.fromTo(subtitleRef.current, { autoAlpha: 0 }, { autoAlpha: 1, duration: .5 });
+      megaTL.fromTo(circleRef.current, { yPercent: 50, y: `${window.innerHeight / 2}`, scale: .7 }, { yPercent: 0, y: 0, scale: 10, duration: 6 }, '-=.7');
+      megaTL.fromTo(megaImageRef.current, { yPercent: 0, autoAlpha: 1 }, { yPercent: -40, duration: 5 }, '-=5.7');
+      megaTL.to(subtitleRef.current, { autoAlpha: 0, duration: .5 });
+      megaTL.fromTo(flagmanItems, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1, stagger: 1 });
 
-      megaTL.to(megaImageRef.current, {yPercent: -50, top: '50%', scale: 2,  duration: 4});
-      megaTL.to(flagmanItems, {autoAlpha: 0, duration: 1.2}, '-=4');
-      
-      megaTL.fromTo(filterRef.current, {autoAlpha: 0}, {autoAlpha: 1, duration: 1}, '-=3');
-      megaTL.fromTo(filterListRef.current, {yPercent: 100}, {yPercent: 0, duration: 3}, '-=2');
+      megaTL.to(megaImageRef.current, { yPercent: -50, top: '50%', scale: 2, duration: 4 });
+      megaTL.to(flagmanItems, { autoAlpha: 0, duration: 1.2 }, '-=4');
 
-      megaTL.fromTo(titleRef.current, {}, {duration: 1});
+      megaTL.fromTo(filterRef.current, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, '-=3');
+      megaTL.fromTo(filterListRef.current, { yPercent: 100 }, { yPercent: 0, duration: 3 }, '-=2');
 
-      megaTL.to(megaImageRef.current, {scale: 1,  duration: 3});
-      megaTL.fromTo(blurRef.current, {autoAlpha: 0}, {autoAlpha: 1, duration: 3}, '-=3');
-      megaTL.to(filterRef.current, {autoAlpha: 0, duration: .3}, '-=3');
+      megaTL.fromTo(titleRef.current, {}, { duration: 1 });
 
-      megaTL.fromTo(firstBlurListRef.current, {autoAlpha: 0, yPercent: 20}, {autoAlpha: 1, yPercent: 0, duration: 3}, '-=2');
-      
-      megaTL.fromTo(titleRef.current, {}, {duration: .3});
+      megaTL.to(megaImageRef.current, { scale: 1, duration: 3 });
+      megaTL.fromTo(blurRef.current, { autoAlpha: 0 }, { autoAlpha: 1, duration: 3 }, '-=3');
+      megaTL.to(filterRef.current, { autoAlpha: 0, duration: .3 }, '-=3');
 
-      megaTL.to(firstBlurListRef.current, {autoAlpha: 0, yPercent: 0, duration: .3});
-      megaTL.fromTo(secondBlurListRef.current, {autoAlpha: 0}, {autoAlpha: 1, yPercent: 0, duration: 3}, '-=.3');
+      megaTL.fromTo(firstBlurListRef.current, { autoAlpha: 0, yPercent: 20 }, { autoAlpha: 1, yPercent: 0, duration: 3 }, '-=2');
 
-      megaTL.fromTo(titleRef.current, {}, {duration: .3});
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
 
-      megaTL.to(secondBlurListRef.current, {autoAlpha: 0, duration: .3});
-      megaTL.fromTo(thirdBlurListRef.current, {autoAlpha: 0}, {autoAlpha: 1, yPercent: 0, duration: 3}, '-=.3');
-      
-      megaTL.fromTo(titleRef.current, {}, {duration: .3});
+      megaTL.to(firstBlurListRef.current, { autoAlpha: 0, yPercent: 0, duration: .3 });
+      megaTL.fromTo(secondBlurListRef.current, { autoAlpha: 0 }, { autoAlpha: 1, yPercent: 0, duration: 3 }, '-=.3');
 
-      megaTL.fromTo(beautyWrapRef.current, {autoAlpha: 0}, {autoAlpha: 1, duration: 2});
-      megaTL.to(megaImageRef.current, {scale: 1.2,  duration: 2}, '-=2');
-      megaTL.to(blurRef.current,{autoAlpha: 0, duration: 1}, '-=.7');
-      megaTL.fromTo(beautyItems, {autoAlpha: 0}, {autoAlpha: 1, duration: 1, stagger: 1});
-      
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
 
-      megaTL.fromTo(titleRef.current, {}, {duration: .3});
-      console.log(beautyItems)
+      megaTL.to(secondBlurListRef.current, { autoAlpha: 0, duration: .3 });
+      megaTL.fromTo(thirdBlurListRef.current, { autoAlpha: 0 }, { autoAlpha: 1, yPercent: 0, duration: 3 }, '-=.3');
+
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+
+      megaTL.fromTo(beautyWrapRef.current, { autoAlpha: 0 }, { autoAlpha: 1, duration: 2 });
+      megaTL.to(megaImageRef.current, { scale: 1.2, duration: 2 }, '-=2');
+      megaTL.to(blurRef.current, { autoAlpha: 0, duration: 1 }, '-=.7');
+      megaTL.fromTo(beautyItems, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1, stagger: 1 });
+
+
+      megaTL.fromTo(titleRef.current, {}, { duration: 1 });
+      megaTL.to(beautyWrapRef.current, { autoAlpha: 0, duration: 1 }, '-=1');
+      megaTL.to(megaImageRef.current, { left: '75%', scale: .9, duration: 2 }, '-=1');
+      megaTL.fromTo(rightSideWrapRef.current, { autoAlpha: 0 }, { autoAlpha: 1, duration: 2 }, '-=1.5');
+      megaTL.fromTo(decisionLetterstRef.current, { autoAlpha: 0, yPercent: 40 }, { autoAlpha: 1, yPercent: 0, duration: 2 }, '-=1');
+      megaTL.fromTo(decisionImgRef.current, { autoAlpha: 0, xPercent: 100 }, { autoAlpha: 1, xPercent: 0, duration: 2 }, '-=1');
+
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+
+      megaTL.to(decisionContentRef.current, { autoAlpha: 0, duration: .5 });
+      megaTL.to(classicContentRef.current, { autoAlpha: 1, duration: .5 }, '-=.5');
+      megaTL.fromTo(classicLetterstRef.current, { autoAlpha: 0, yPercent: 40 }, { autoAlpha: 1, yPercent: 0, duration: 2 }, '-=1');
+      megaTL.fromTo(classicImgRef.current, { autoAlpha: 0, xPercent: 100 }, { autoAlpha: 1, xPercent: 0, duration: 2 }, '-=1');
+
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+
+      megaTL.to(classicContentRef.current, { autoAlpha: 0, duration: .5 });
+      megaTL.to(elementaryContentRef.current, { autoAlpha: 1, duration: .5 }, '-=.5');
+      megaTL.fromTo(elementaryLetterstRef.current, { autoAlpha: 0, yPercent: 40 }, { autoAlpha: 1, yPercent: 0, duration: 2 }, '-=1');
+      megaTL.fromTo(elementaryImgRef.current, { autoAlpha: 0, xPercent: 100 }, { autoAlpha: 1, xPercent: 0, duration: 2 }, '-=1');
+
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+
+      megaTL.to(elementaryContentRef.current, { autoAlpha: 0, duration: .5 });
+      megaTL.to(energyContentRef.current, { autoAlpha: 1, duration: .5 }, '-=.5');
+      megaTL.fromTo(energyLetterstRef.current, { autoAlpha: 0, yPercent: 40 }, { autoAlpha: 1, yPercent: 0, duration: 2 }, '-=1');
+      megaTL.fromTo(energyImgRef.current, { autoAlpha: 0, xPercent: 100 }, { autoAlpha: 1, xPercent: 0, duration: 2 }, '-=1');
+
+      megaTL.fromTo(titleRef.current, {}, { duration: .3 });
+
+      megaTL.to(energyContentRef.current, { autoAlpha: 0, duration: .5 });
+      megaTL.fromTo(lastBlocktRef.current, {autoAlpha: 0}, {autoAlpha: 1, duration: .5}, '-=.5');
+      megaTL.to(megaImageRef.current, { left: '50%', scale: 1, duration: 2 }, '-=.5');
+      megaTL.fromTo(lastItems, { autoAlpha: 0, y: 100 }, { autoAlpha: 1, y: 0, duration: 1, stagger: 1 });
+
+      megaTL.fromTo(titleRef.current, {}, { duration: 1 });
 
       ScrollTrigger.create({
         trigger: sectionRef.current,
         start: 'top top',
         end: 'bottom bottom',
         animation: megaTL,
-        scrub: 1, 
+        scrub: 1,
         markers: false,
         // pin: pinnedRef.current,
         pinSpacing: false
@@ -90,7 +144,7 @@ const MegaPower: React.FC = () => {
 
   return (
     // если анимация выполняется слишком быстро или медленно, отрегулируйте минимальную высоту секции
-    <section className="min-h-[800vh] bg-white-2"  ref={sectionRef}>
+    <section className="min-h-[1300vh] bg-white-2" ref={sectionRef}>
       <div className="sticky top-0 flex h-[100vh] min-h-[100vh] overflow-clip bg-white" ref={pinnedRef}>
         <div
           ref={circleRef}
@@ -110,12 +164,12 @@ const MegaPower: React.FC = () => {
         </div>
         <div ref={flagmanRef} className="absolute left-0 top-0 h-[100vh] z-[6] text-white">
           <h2 className="js-megaPower-flagman-item">
-              Флагман линейки — <br/>
-              там где нужна гибкость
-            </h2>
-            <p className="js-megaPower-flagman-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa nobis ad, magnam, ab, eum excepturi maxime fuga delectus a totam.</p>
-            <div className="js-megaPower-flagman-item">lG LTE</div>
-            <div className="js-megaPower-flagman-item">Тут иконки</div>
+            Флагман линейки — <br />
+            там где нужна гибкость
+          </h2>
+          <p className="js-megaPower-flagman-item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsa nobis ad, magnam, ab, eum excepturi maxime fuga delectus a totam.</p>
+          <div className="js-megaPower-flagman-item">lG LTE</div>
+          <div className="js-megaPower-flagman-item">Тут иконки</div>
         </div>
         <div
           ref={megaImageRef}
@@ -171,6 +225,88 @@ const MegaPower: React.FC = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto corrupti aspernatur mollitia omnis? Voluptate
           </p>
         </div>
+
+        <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex items-center justify-center invisible backdrop-brightness-75" ref={rightSideWrapRef}>
+          <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex justify-between" ref={decisionContentRef}>
+            <div className="w-[50vw] text-white" ref={decisionLetterstRef}>
+              Модуль расширения радиоканала 868
+              Решение, не имеющее аналогов. Стены — для вас не помеха.
+              Расстояния - только для преодоления. Мы сделали возможной передачу сигнала датчик-прибор на расстоянии 5000 м. Без ретрансляторов.
+
+              5-6 капитальных перекрытий - именно такая пробиваемость радиоканала.
+            </div>
+            <div className="relative h-[100vh] w-[50vw]" ref={decisionImgRef}>
+              <Image
+                layout="fill"
+                objectFit="contain"
+                src={"/ext1-1600px.png"}
+                alt="Mega_1"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex justify-between invisible" ref={classicContentRef}>
+          <div className="w-[50vw] text-white" ref={classicLetterstRef}>
+            Классика, как вы
+            и хотели. Когда нет смысла платить
+            за лишнее. Классика, как вы
+            и хотели. Когда нет смысла платить
+            за лишнее. Классика, как вы
+            и хотели. Когда нет смысла платить
+            за лишнее.
+          </div>
+          <div className="relative h-[100vh] w-[50vw]" ref={classicImgRef}>
+            <Image
+              layout="fill"
+              objectFit="contain"
+              src={"/ext2-1600px.png"}
+              alt="Mega_1"
+            />
+          </div>
+        </div>
+
+        <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex justify-between invisible" ref={elementaryContentRef}>
+          <div className="w-[50vw] text-white" ref={elementaryLetterstRef}>
+            Несмотря на наличие штатного Ethernet,
+            не ограничивайте себя.  Несмотря на наличие штатного Ethernet,
+            не ограничивайте себя. Несмотря на наличие штатного Ethernet,
+            не ограничивайте себя.
+          </div>
+          <div className="relative h-[100vh] w-[50vw]" ref={elementaryImgRef}>
+            <Image
+              layout="fill"
+              objectFit="contain"
+              src={"/ext3-1600px.png"}
+              alt="Mega_1"
+            />
+          </div>
+        </div>
+
+        <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex justify-between invisible" ref={energyContentRef}>
+          <div className="w-[50vw] text-white" ref={energyLetterstRef}>
+            Казалось бы, причём тут счетчики электроэнергии? Казалось бы, причём тут счетчики электроэнергии? Казалось бы, причём тут счетчики электроэнергии?
+          </div>
+          <div className="relative h-[100vh] w-[50vw]" ref={energyImgRef}>
+            <Image
+              layout="fill"
+              objectFit="contain"
+              src={"/ext4-1600px.png"}
+              alt="Mega_1"
+            />
+          </div>
+        </div>
+
+        
+        <div className="z-[5] absolute bottom-0 left-0 top-0 right-0 flex items-center justify-center invisible" ref={lastBlocktRef}>
+          <h2 className="text-white js-last-item">
+          А ещё, мы сразу комплектуем прибор надежным блоком питания
+          </h2>
+          <p className="absolute bottom-5 m-x-auto text-white js-last-item">
+          Прошел все необходимые испытания при сертификации CE
+          </p>
+        </div>
+
       </div>
 
     </section>
